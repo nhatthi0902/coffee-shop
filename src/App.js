54c93f0e536bd0ee4b67a6f3d002f73e3879c0e8
nhat thi <dinhnhatthi1996@gmail.com>
2021-10-29 18:28:30 +0700

    Init source

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import store from "./store";
+import "./assets/scss/base.scss";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Home from "./page/Home";
+import Menu from "./page/Menu";
+import Cart from "./page/Cart";
+import Booking from "./page/Booking";
+import Location from "./page/Location";
+import Login from "./page/Login";
+import Register from "./page/Register";
+import Order from "./page/Order";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <main>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/menu">
+              <Menu />
+            </Route>
+            <Route path="/booking">
+              <Booking />
+            </Route>
+            <Route path="/cart">
+              <Cart />
+            </Route>
+            <Route path="/location">
+              <Location />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/order">
+              <Order />
+            </Route>
+          </Switch>
+          <Footer />
+        </main>
+      </Router>
+    </Provider>
   );
 }
 
